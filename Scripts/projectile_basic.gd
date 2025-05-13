ce00bfb4bfefbefdcada66fd7554843b182df70f
Arman Ameri <arman.ameri48+Github@gmail.com>
2025-05-13 11:16:33 +0200

    Spawner init

--- Scripts/projectile_basic.gd
+++ Scripts/projectile_basic.gd
@@ -4,7 +4,7 @@ extends RayCast3D
 
 @onready var remote_transform: RemoteTransform3D = RemoteTransform3D.new()
 
-var damage: int = 15
+var damage: int = 20
 
 func _physics_process(delta: float) -> void:
 	position += global_basis * Vector3.FORWARD * speed * delta
