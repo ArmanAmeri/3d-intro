a4eb849fd94a1e981f1abb8adc507ccc6b195df1
Eimantasas <152972605+Eimantasas@users.noreply.github.com>
2025-05-20 13:23:58 +0200

    Adding a domain expansion

---
+++ Scripts/malevolent_shrine.gd
@@ -0,0 +1,13 @@
+extends Node3D
+
+@onready var anim_p: AnimationPlayer = $AnimationPlayer
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	anim_p.play("domain_expansion")
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
