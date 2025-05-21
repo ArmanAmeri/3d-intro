6f0669c2c920e22599f1aa5de2f314a9f1353a8c
Eimantasas <152972605+Eimantasas@users.noreply.github.com>
2025-05-21 15:02:00 +0200

    Working on dismantle

--- Characters/Sukuna/Attacks/dismantle.gd
+++ Characters/Sukuna/Attacks/dismantle.gd
@@ -1,11 +1,25 @@
 extends Node3D
 
-var speed : float = 90.0
-var damage: int = 20
+var speed : float = 40.0
+var damage: int = 50
+
+@export var lifetime: float = 10
+
+@onready var cleanup_timer: Timer = $Timers/CleanupTimer
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass
+	cleanup_timer.start()
 
 func _process(delta: float) -> void:
 	position += global_basis * Vector3.FORWARD * speed * delta
+
+
+func cleanup() -> void:
+	queue_free()
+
+
+func _on_area_3d_area_entered(area: Area3D) -> void:
+	if area.has_method("take_damage"):
+		area.take_damage(damage)
--- Scripts/player.gd
+++ Scripts/player.gd
@@ -90,6 +90,9 @@ func _physics_process(delta):
 	if Input.is_action_just_pressed("shoot"):
 		Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
 	
+	if Input.is_action_just_pressed("Attack1"):
+		pass
+	
 	if Input.is_action_just_pressed("Special"):
 		used_special.emit()
 		continuous_laser.visible = true
